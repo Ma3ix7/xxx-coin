01e65d9a5b3b9a91a844446b85f71794b2a34ebd
egorkobzev23@gmail.com <92168210+PrintHiEgProg@users.noreply.github.com>
2024-06-10 21:37:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  // Загружаем сохраненное значение count из localStorage при инициализации состояния
+  const [count, setCount] = useState(() => {
+    const savedCount = localStorage.getItem("count");
+    return savedCount !== null ? parseInt(savedCount, 10) : 0;
+  });
+
+  useEffect(() => {
+    // Сохраняем значение count в localStorage при его изменении
+    localStorage.setItem("count", count);
+  }, [count]); // Зависимость от переменной count, чтобы эффект срабатывал при её изменении
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h2 className="title">XXX Coin</h2>
+      <p className="count-click">{count}</p>
+      <div className="coin-btn">
+        <a className="" onClick={handleClick}>
+          <img
+            className="img-boobs"
+            src="https://i.postimg.cc/5NbYNHhq/boobs.jpg"
+            alt="boobs"
+          />
         </a>
-      </header>
+        <speacer>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </speacer>
+      </div>
     </div>
   );
 }
