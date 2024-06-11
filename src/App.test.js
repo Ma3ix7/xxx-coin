fbe58ea202fa849aad7f14bc3c789996780e9eb6
Ma3ix7 <kriovida@gmail.com>
2024-06-11 05:01:34 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders the title "XXX Coin"', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const titleElement = screen.getByText(/XXX Coin/i);
-  expect(titleElement).toBeInTheDocument();
-});
-
-test('renders the click counter', () => {
-  render(<App />);
-  const counterElement = screen.getByText(/0/); // Предполагая, что начальное значение счётчика 0
-  expect(counterElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
